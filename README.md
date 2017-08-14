79106b1ac18078799858b1a4a74074dcb48650b8
李铭昕 <715557344@qq.com>
2017-08-14 09:32:45 +0800

    * init

---
+++ README.md
@@ -0,0 +1,8 @@
+## Github-Api 类
+
+### 安装
+~~~
+composer require limingxinleo/github-api
+~~~
+
+
---
+++ src/Github/Application.php
@@ -0,0 +1,80 @@
+<?php
+// +----------------------------------------------------------------------
+// | Redis类 [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+namespace limx\Github;
+
+use Pimple\Container;
+use Config;
+
+/**
+ * Class Application
+ * @package limx\Github
+ * @property Opt      $opt
+ * @property Client   $client
+ * @property Response $response
+ */
+class Application extends Container
+{
+    /**
+     * Service Providers.
+     *
+     * @var array
+     */
+    protected $providers = [
+        ServiceProviders\OptServiceProvider::class,
+        ServiceProviders\ClientServiceProvider::class,
+        ServiceProviders\ResponseServiceProvider::class,
+    ];
+
+    /**
+     * Application constructor.
+     *
+     * @param array $config
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->registerProviders();
+
+    }
+
+
+    /**
+     * Magic get access.
+     *
+     * @param string $id
+     *
+     * @return mixed
+     */
+    public function __get($id)
+    {
+        return $this->offsetGet($id);
+    }
+
+    /**
+     * Magic set access.
+     *
+     * @param string $id
+     * @param mixed  $value
+     */
+    public function __set($id, $value)
+    {
+        $this->offsetSet($id, $value);
+    }
+
+    /**
+     * Register providers.
+     */
+    private function registerProviders()
+    {
+        foreach ($this->providers as $provider) {
+            $this->register(new $provider());
+        }
+    }
+}
